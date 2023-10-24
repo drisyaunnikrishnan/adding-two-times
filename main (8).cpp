f1ca22129219e32ab4bfe8cb3899fe55206d212b
Drisya unnikrishnan <134525521+drisyaunnikrishnan@users.noreply.github.com>
2023-10-24 17:02:04 +0530

    Add files via upload

---
+++ main (8).cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class Time
+{
+ int seconds;
+ int minutes;
+ int hours;
+
+ public:
+
+
+ void read()
+ {
+ cin>>hours>>minutes>>seconds;
+
+
+ }
+
+ Time operator +(Time t2)
+ {
+ int carry;
+ Time sum;
+ sum.seconds=(seconds+t2.seconds)%60;
+ carry=(seconds+t2.seconds)/60;
+ sum.minutes=(minutes+t2.minutes+carry)%60;
+ carry=(minutes+t2.minutes+carry)/60;
+ sum.hours=(hours+t2.hours+carry)%24;
+ return sum;
+
+ }
+
+ void display()
+ {
+ cout<<"SUM = "<<hours <<" hours : "<<minutes<<" minutes : "<<seconds<<" seconds ";
+ }
+
+};
+int main()
+{
+ Time obj1,obj2;
+
+ cout<<" enter the first time (in HH:MM:SS ) : ";
+ obj1.read();
+ cout<<" enter the second time (in HH:MM:SS ) : ";
+ obj2.read();
+
+ Time result =obj1+obj2;
+
+ result.display();
+
+}
